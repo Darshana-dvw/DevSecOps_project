40b634fc21e0cb1dd93d615235c7286374a05678
Darshana-dvw <132748034+Darshana-dvw@users.noreply.github.com>
2025-11-22 05:22:50 +0530

    updated

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "deployer" {
   key_name   = "terra-automate-key"
-  public_key = file("/Users/shubham/Documents/work/TrainWithShubham/terra-practice/terra-key.pub")
+  public_key = file("/home/ubuntu/DevSecOps_Project/terraform/terraform-key.pub")
 }
 
 resource "aws_default_vpc" "default" {
