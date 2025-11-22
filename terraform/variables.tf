cec9e59febddd387c93671b4bb34719b6c77d453
Darshana-dvw <132748034+Darshana-dvw@users.noreply.github.com>
2025-11-22 05:44:44 +0530

    updated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "AWS region where resources will be provisioned"
-  default     = "us-east-2"
+  default     = "ap-southeast-2"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  default     = "ami-085f9c64a9b75eed5"
+  default     = "ami-025ca978d4c1d9825"
 }
 
 variable "instance_type" {
